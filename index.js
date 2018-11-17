fc95a9d734aacd34c0e2e0b1c8f049ae88a4d0f6
Louie O'Grady <l.ogrady@gmail.com>
2018-11-17 17:17:14 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ function nowServing(katzDeli) {
 var line = [];
 
 function currentLine(katzDeli) {
-  let i = 0;
+  var i = 0;
   while (i < katzDeli.length) {
     line.push(` `+[i+1]+`. `  + katzDeli[i]);
     i++;
